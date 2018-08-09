4cc20593b940cbca0c8bc53f62d30b443263d5e5
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-08-09 18:16:13 +0300

    ko observable

--- Knockout/Scripts/MyScript1.js
+++ Knockout/Scripts/MyScript1.js
@@ -1,16 +1,28 @@
 console.log('test');
 
 var ViewModel = function(first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
- 
-    this.fullName = ko.computed(function() {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
-    }, this);
+  this.firstName = ko.observable(first);
+  this.lastName = ko.observable(last);
+
+  this.fullName = ko.computed(function() {
+    // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+    return this.firstName() + " " + this.lastName();
+  }, this);
+
+  this.myClickHandler = function() {
+    debugger;
+    console.log('my handler click');
+  }
+  this.personName = ko.observable('Bob');
+  this.personAge = ko.observable(123);
 };
 
-window.onload=function(){
-	console.log('wnd on load');
-	ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+window.onload = function() {
+  console.log('wnd on load');
+  ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+  let ctx = ko.contextFor(document.getElementById('mydiv1'));
+  ctx.$data.personName('testname');
+  ctx.$data.firstName.subscribe(function(newValue){
+    console.log('person name changed ='+newValue);
+  })
 };
