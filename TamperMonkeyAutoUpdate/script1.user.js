ff5155abcb31ea57ff37475c151694657d41c589
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-08-13 18:08:16 +0300

    tamp2

--- TamperMonkeyAutoUpdate/script1.user.js
+++ TamperMonkeyAutoUpdate/script1.user.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         Tamp auto update
+// @namespace    http://tampermonkey.net/
+// @version      0.1.1
+// @description  test auto update
+// @author       AndreyK
+// @match        https://isc.devexpress.com/Thread/WorkplaceDetails*
+// @grant        none
+// ==/UserScript==
+
 window.onload=function(){
   console.log('myautoupdate1');
 }
