ed036e8f62820f55106fa760ae620120c6285e46
Andrey <freazeek@mail.ru>
2020-03-24 17:48:20 +0300

    test

--- Promise/myscript.js
+++ Promise/myscript.js
@@ -7,16 +7,26 @@ function void1(){
 }
 function void2(){
    return new Promise(function(resolve, reject) {
-    setTimeout(() => resolve("done2!"), 1000);
+    setTimeout(() => resolve("done2!"), 2000);
   });
 }
 function void3(){
    return new Promise(function(resolve, reject) {
-    setTimeout(() => resolve("done3!"), 1000);
+    setTimeout(() => resolve("done3!"), 3000);
   });
 }
 
-void1()
-  .then(r=>{console.log(r);return void2();})
-  .then(r=>{console.log(r);return void3();})
-  .then(r=>console.log(r));
+// void1()
+//   .then(r=>{console.log(r);return void2();})
+//   .then(r=>{console.log(r);return void3();})
+//   .then(r=>console.log(r));
+void1().then(r=>console.log(r));
+void2().then(r=>console.log(r));
+void3().then(r=>console.log(r));
+Promise.all([
+  void3(),
+   void1(),
+   void2()
+   
+  
+]).then(alert);
